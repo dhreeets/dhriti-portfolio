7f6efd1c7e14169e590446c22f8b1de63ded8281
Dhriti Sharma <dhritisharma.2107@gmail.com>
2026-09-02 00:08:33 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return(
+    <div className = "cover-background">
+      <div className = "name">Dhriti Sharma</div>
+      <div className = "phonetic">/dhree-tih/shur-mah/</div>
+      <div className = "below-name">Personal Portfolio</div>
+    </div>
+  )
+}
+
+export default App
