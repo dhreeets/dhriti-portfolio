966fe3ff3877a9774019f633f6d6952de9165a80
Dhriti Sharma <dhritisharma.2107@gmail.com>
2026-09-03 19:46:05 +0100

    adding phone details

--- src/ContactInfo.tsx
+++ src/ContactInfo.tsx
@@ -1,11 +1,16 @@
 import emailIcon from './laceEmailIcon.png';
+import phoneIcon from './lacePhoneIcon.png';
 
 function ContactInfo() {
     return (
-        <div className="email-info">
+        <div className="contact-info">
             <img src={emailIcon} alt="unable to load email icon" 
                 width="55px" height="auto"/>
             <span>dhritisharma.2107@gmail.com</span>
+
+            <img src={phoneIcon} alt="unable to load phone icon" 
+                width="55px" height="auto"/>
+            <span>+44 7880 569494 </span>
         </div>
     )
 }
