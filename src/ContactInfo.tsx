b6f073dcc9792219666baa2b68f3b4ed200f6b7a
Dhriti Sharma <dhritisharma.2107@gmail.com>
2026-09-05 15:03:57 +0100

    adding postcards for work exp

--- src/ContactInfo.tsx
+++ src/ContactInfo.tsx
@@ -5,16 +5,25 @@ import linkedInIcon from './linkedInIcon.png';
 function ContactInfo() {
     return (
         <div className="contact-info">
-            <img src={emailIcon} alt="unable to load email icon" 
-                width="65px" height="auto"/>
+            <img 
+                src={emailIcon} 
+                alt="unable to load email icon" 
+                width="65px" 
+                height="auto"/>
             <span>dhritisharma.2107@gmail.com</span>
 
-            <img src={phoneIcon} alt="unable to load phone icon" 
-                width="65px" height="auto"/>
+            <img 
+                src={phoneIcon} 
+                alt="unable to load phone icon" 
+                width="65px" 
+                height="auto"/>
             <span>+44 7880 569494 </span>
 
-            <img src={linkedInIcon} alt="unable to load linkedIn icon"
-                width="65px" height="auto"/>
+            <img 
+                src={linkedInIcon} 
+                alt="unable to load linkedIn icon"
+                width="65px" 
+                height="auto"/>
             <span>linkedin.com/in/dhriti7/</span>
         </div>
     )
--- src/WorkExperience.tsx
+++ src/WorkExperience.tsx
@@ -1,6 +1,26 @@
+import jpmcPostcard from './JpmcPostcard.png';
+import uniOfLivPostcard from './UniOfLivPostcard.png';
+
 function WorkExperience() {
     return (
-        <div className="work-experience">Work Experience</div>
+        <div className="work-experience">Work Experience
+            <div className="images">
+                <img 
+                    className="jpmc-postcard"
+                    src={jpmcPostcard} 
+                    alt="unable to load jpmc postcard"
+                    width="200px" 
+                    height="auto"/>
+
+                <img
+                    className="uni-of-liv-postcard"
+                    src={uniOfLivPostcard}
+                    alt="unable to load uni of liv postcard"
+                    width="200px" 
+                    height="auto"
+                />
+            </div>
+        </div>
     )
 }
 
