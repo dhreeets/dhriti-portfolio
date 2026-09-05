5d9b59d83f406fc5f45af523c78ce90fb424f4f8
Dhriti Sharma <dhritisharma.2107@gmail.com>
2026-09-05 21:06:30 +0100

    job titles

--- src/WorkExperience.tsx
+++ src/WorkExperience.tsx
@@ -8,39 +8,54 @@ function WorkExperience() {
     return (
         <div className="work-experience">Work Experience
             <div className="image-row">
-                <img 
+                <div className="image-item">
+                    <img 
                     src={jpmcPostcard} 
                     alt="unable to load jpmc postcard"
                     width="200px" 
                     height="auto"/>
+                    <span>Software Engineering Industrial Placement</span>
+                </div>
+                
+                <div className="image-item">
+                    <img
+                        src={uniOfLivPostcard}
+                        alt="unable to load uni of liv postcard"
+                        width="200px" 
+                        height="auto"
+                    />
+                    <span>President, Girls in Coding</span>
+                </div>
 
-                <img
-                    src={uniOfLivPostcard}
-                    alt="unable to load uni of liv postcard"
-                    width="200px" 
-                    height="auto"
-                />
+                <div className="image-item">
+                    <img
+                        src={tutorDoctorPostcard}
+                        alt="unable to load tutor doctor postcard"
+                        width="200px" 
+                        height="auto"
+                    />
+                    <span>GCSE Maths Tutor</span>
+                </div>
 
-                <img
-                    src={tutorDoctorPostcard}
-                    alt="unable to load tutor doctor postcard"
-                    width="200px" 
-                    height="auto"
-                />
+                <div className="image-item">
+                    <img
+                        src={externPostcard}
+                        alt="unable to load extern postcard"
+                        width="200px" 
+                        height="auto"
+                    />
+                    <span>AI Market Risk Research Externship</span>
+                </div>
 
-                <img
-                    src={externPostcard}
-                    alt="unable to load extern postcard"
-                    width="200px" 
-                    height="auto"
-                />
-
-                <img
-                    src={springpodPostcard}
-                    alt="unable to load springpod postcard"
-                    width="200px" 
-                    height="auto"
-                />
+                <div className="image-item">
+                    <img
+                        src={springpodPostcard}
+                        alt="unable to load springpod postcard"
+                        width="200px" 
+                        height="auto"
+                    />
+                    <span>Software Engineering Virtual Work Experience</span>
+                </div>
 
             </div>
         </div>
