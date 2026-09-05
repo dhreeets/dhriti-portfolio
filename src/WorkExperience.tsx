88a14896372ad9ca8c218e3d373419aad598c1cc
Dhriti Sharma <dhritisharma.2107@gmail.com>
2026-09-05 20:08:09 +0100

    additional postcards

--- src/WorkExperience.tsx
+++ src/WorkExperience.tsx
@@ -1,21 +1,27 @@
 import jpmcPostcard from './JpmcPostcard.png';
 import uniOfLivPostcard from './UniOfLivPostcard.png';
+import tutorDoctorPostcard from './TutorDoctorPostcode.png';
 
 function WorkExperience() {
     return (
         <div className="work-experience">Work Experience
             <div className="images">
                 <img 
-                    className="jpmc-postcard"
                     src={jpmcPostcard} 
                     alt="unable to load jpmc postcard"
                     width="200px" 
                     height="auto"/>
 
                 <img
-                    className="uni-of-liv-postcard"
                     src={uniOfLivPostcard}
                     alt="unable to load uni of liv postcard"
+                    width="200px" 
+                    height="auto"
+                />
+
+                <img
+                    src={tutorDoctorPostcard}
+                    alt="unable to load tutor doctor postcard"
                     width="200px" 
                     height="auto"
                 />
